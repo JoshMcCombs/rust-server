ed1f6a93eebf6a9413b6ac6b7323659be7b4fdcc
Josh <joshua.lee.mccombs@gmail.com>
2021-02-14 12:09:38 -0600

    began work on server, assigning port #

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // let server = Server::new("127.0.0.1:8005");
+    // Server.run();
+    let string = String::from("127.0.0.1:8005");
+    let str_slice = &string[13..]
+}
+
+struct Server {
+    addr: String,
+} 
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Server {
+            addr: addr
+        }
+    }
+
+    fn run(self) {
+
+    }
 }
