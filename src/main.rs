9637e097cb2a0efcdce90f6dac710d2fcaf65b2a
Josh <joshua.lee.mccombs@gmail.com>
2021-02-25 22:39:56 -0600

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
 fn main() {
-    println!("Hello, world!");
+  let server = server::Server::new("127.0.0.1:8899".to_string());
+  server.listen();
+}
+
+mod server {
+  pub struct Server {
+    address: String,
+  }
+
+  impl Server {
+    pub fn new(address: String) -> Self {
+      Self { address }
+    }
+    pub fn listen(self) {
+      println!("Listening on port {}", self.address);
+    }
+  }
+}
+struct Request {
+  path: String,
+  query_string: Option<String>,
+  method: Method,
+}
+
+enum Method {
+  GET,
+  POST,
+  DELETE,
+  PUT,
+  HEAD,
+  CONNECT,
+  OPTIONS,
+  TRACE,
+  PATCH,
 }
