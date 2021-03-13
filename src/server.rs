311898075b33c46236b6374922afe8d17f45be17
Josh <joshua.lee.mccombs@gmail.com>
2021-03-13 10:00:29 -0600

    changed listner buffer size

--- src/server.rs
+++ src/server.rs
@@ -15,7 +15,7 @@ impl Server {
     loop {
       match listener.accept() {
         Ok((mut stream, _)) => {
-          let mut buffer = [0; 1024];
+          let mut buffer = [0; 4 * 1024];
           match stream.read(&mut buffer) {
             Ok(_) => println!("Received a request: {}", String::from_utf8_lossy(&buffer)),
             Err(e) => println!("Stream read error: {:?}", e)
